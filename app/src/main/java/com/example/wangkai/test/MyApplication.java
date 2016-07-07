2aa19a7384027e6ff0800f17b29138a85df7af85
王凯 <xiaokai@maihaoche.com>
2016-07-07 10:53:07 +0800

    获取手机宽高

--- app/src/main/java/com/example/wangkai/test/MyApplication.java
+++ app/src/main/java/com/example/wangkai/test/MyApplication.java
@@ -1,6 +1,10 @@
 package com.example.wangkai.test;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.app.Activity;
+import android.view.WindowManager;
 
 /**
  * Created by wangkai on 16/7/7.
@@ -14,12 +18,6 @@ public class MyApplication extends Application{
         super.onCreate();
         mMyApplication = this;
     }
-
-    /*
-    public synchronized MyApplication getInstance(){
-        return mMyApplication;
-    }
-    */
     public MyApplication getInstance(){
         if(mMyApplication == null){
             synchronized (MyApplication.class){
@@ -31,4 +29,10 @@ public class MyApplication extends Application{
         return mMyApplication;
     }
 
+    public void getPhoneScreenWidth(){
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        int screenWidth = wm.getDefaultDisplay().getWidth();
+        int screenHeight = wm.getDefaultDisplay().getHeight();
+        
+    }
 }
